a15a6876754248e6d962b2b17ea85009cfc35315
Muhammad Ali <muhammad.ali@vu360solutions.org>
2024-08-12 11:52:00 +0500

    fix endpoint template

--- FastEndpointsTool/Program.cs
+++ FastEndpointsTool/Program.cs
@@ -1,6 +1,7 @@
 using FastEndpointsTool.Generator;
 using FastEndpointsTool.Parsing;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace FastEndpointsTool;
 
@@ -8,28 +9,25 @@ internal class Program
 {
     static async Task Main(string[] args)
     {
-        var versionString = Assembly.GetEntryAssembly()?
-                                    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
-                                    .InformationalVersion
-                                    .ToString();
         if (args.Length == 0)
         {
-            Console.WriteLine($"fetool v{versionString}");
+            Console.WriteLine($"FastEndpointsTool Version v{GetVersion()}");
             Console.WriteLine("-------------");
             Console.WriteLine("\nUsage:");
             ShowHelp();
             return;
         }
-        //if (args.Length == 1 && (args[0] == "--help" || args[0] == "-h"))
-        //{
-        //    ShowHelp();
-        //    return;
-        //}
-        //if (args.Length == 1 && (args[0] == "--version" || args[0] == "-v"))
-        //{
-        //    Console.WriteLine($"fetool v{versionString}");
-        //    return;
-        //}
+        if (args.Length == 1 && (args[0] == "--help" || args[0] == "-h"))
+        {
+            Console.WriteLine("Usage:");
+            ShowHelp();
+            return;
+        }
+        if (args.Length == 1 && (args[0] == "--version" || args[0] == "-v"))
+        {
+            Console.WriteLine($"FastEndpointsTool Version v{GetVersion()}");
+            return;
+        }
 
         var argument = new Parser().Parse(args);
         await new CodeGenerator().Generate(argument);
@@ -37,16 +35,36 @@ internal class Program
 
     static void ShowHelp()
     {
-        Console.WriteLine("  fetool help");
+        Console.WriteLine("  use like this");
+        Console.WriteLine("  fet {command} {options}");
         Console.WriteLine("  name, shortname, description");
         var arguments = ArgumentInfo.Arguments();
         foreach (var arg in arguments)
         {
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("  command");
             Console.WriteLine($"  {arg.Name}, {arg.ShortName}, {arg.Description}");
+            Console.WriteLine("  options");
             foreach (var opt in arg.Options)
             {
                 Console.WriteLine($"    {opt.Name}, {opt.ShortName}, {opt.Description}");
             }
         }
+    }
+
+    static string GetVersion()
+    {
+        var versionString = Assembly.GetEntryAssembly()?
+                                    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                                    .InformationalVersion
+                                    .ToString();
+        if (string.IsNullOrEmpty(versionString))
+            throw new Exception("No version found.");
+        var pattern = @"\d+\.\d+\.\d+";
+        var match = Regex.Match(versionString, pattern);
+        if (match.Success)
+            return match.Value;
+        else
+            throw new Exception("No version found.");
     }
 }
