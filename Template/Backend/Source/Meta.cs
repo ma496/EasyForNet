3b1d055d67f0bd6ff1513bbd7dc1ad99787c35af
Muhammad Ali <ma496@outlook.com>
2024-11-11 23:26:53 +0500

    initial backend template

---
+++ Template/Backend/Source/Meta.cs
@@ -0,0 +1,8 @@
+﻿global using FastEndpoints;
+global using FastEndpoints.Security;
+global using FastEndpoints.Swagger;
+global using Backend;
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Tests")]
---
+++ Template/Backend/Source/Program.cs
@@ -0,0 +1,20 @@
+var bld = WebApplication.CreateBuilder(args);
+bld.Services
+   .AddAuthenticationJwtBearer(s => s.SigningKey = bld.Configuration["Auth:JwtKey"])
+   .AddAuthorization()
+   .AddFastEndpoints(o => o.SourceGeneratorDiscoveredTypes = DiscoveredTypes.All)
+   .SwaggerDocument();
+
+var app = bld.Build();
+app.UseAuthentication()
+   .UseAuthorization()
+   .UseFastEndpoints(
+       c =>
+       {
+           c.Binding.ReflectionCache.AddFromBackend();
+           c.Errors.UseProblemDetails();
+       })
+   .UseSwaggerGen();
+app.Run();
+
+public partial class Program { }
