d3fdce1d6baedd7423c8f1e053c1015ea9576aef
ma496 <ma496@outlook.com>
2024-12-22 20:21:40 +0500

    delete web project

--- Template/Frontend/fe-web/global.d.ts
+++
@@ -1,5 +0,0 @@
-// Use type safe message keys with `next-intl`
-type GlobalMessages = typeof import('./messages/en.json');
-type ZodMessages = typeof import('./messages/zod/en.json');
-type Messages = GlobalMessages & ZodMessages;
-declare interface IntlMessages extends Messages {}
--- Template/Frontend/fe-web/lib/constants.ts
+++
@@ -1,3 +0,0 @@
-export const localeStorageConst = {
-  signIn: 'signIn',
-}
