01f5bec37888021bd269b3b1ccf7fa037fda5b50
ma496 <ma496@outlook.com>
2025-03-20 02:40:36 +0500

    nav items

---
+++ Template/Frontend/fe-web/nav-items.ts
@@ -0,0 +1,74 @@
+import { Users, Shield, Settings, Bug, Building, Home, User, Lock } from "lucide-react"
+
+export type NavItemGroup = {
+  title: string
+  items: NavItem[]
+}
+
+export type NavItem = {
+  title: string
+  url: string
+  icon?: any
+  badge?: number
+  group?: string
+  show?: boolean
+  isActive?: boolean
+  children?: NavItem[]
+}
+
+export const navItems: (NavItem | NavItemGroup)[] = [
+  {
+    title: 'Dashboard',
+    url: '/',
+    icon: Home,
+    children: [
+      {
+        title: 'Dashboard',
+        url: '/',
+        icon: Home,
+      }
+    ]
+  },
+  {
+    title: 'Administration',
+    items: [
+      {
+        title: 'Users',
+        url: '/users',
+        icon: Users,
+      },
+      {
+        title: 'Roles',
+        url: '/roles',
+        icon: Shield,
+      },
+      {
+        title: 'Settings',
+        url: '/settings',
+        icon: Settings,
+      },
+      {
+        title: 'Errors',
+        url: '/errors',
+        icon: Bug,
+      },
+      {
+        title: 'Tenants',
+        url: '/tenants',
+        icon: Building,
+      },
+      {
+        title: 'ChangePassword',
+        url: '/change-password',
+        icon: Lock,
+        show: false,
+      },
+      {
+        title: 'Profile',
+        url: '/profile',
+        icon: User,
+        show: false,
+      },
+    ]
+  }
+]
