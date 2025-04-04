b1ab296d1d87ba6d3ea35f5da5abebce11a8af64
ma496 <ma496@outlook.com>
2025-04-05 02:50:43 +0500

    user demo table

--- Template/Frontend/fe-web/app/(defaults)/users/list/_components/user-table.tsx
+++ Template/Frontend/fe-web/app/(defaults)/users/list/_components/user-table.tsx
@@ -1,6 +1,7 @@
 'use client';
-import { DataTable } from 'mantine-datatable';
+import { DataTable, DataTableSortStatus } from 'mantine-datatable';
 import { useEffect, useState } from 'react';
+import sortBy from 'lodash/sortBy';
 
 const rowData = [
   {
@@ -504,14 +505,20 @@ const rowData = [
     company: 'PHARMACON',
   },
 ];
-
 export const UserTable = () => {
   const [page, setPage] = useState(1);
   const PAGE_SIZES = [10, 20, 30, 50, 100];
   const [pageSize, setPageSize] = useState(PAGE_SIZES[0]);
-  const initialRecords = rowData.slice(0, pageSize);
+  const [initialRecords, setInitialRecords] = useState(sortBy(rowData, 'id'));
   const [recordsData, setRecordsData] = useState(initialRecords);
+
   const [selectedRecords, setSelectedRecords] = useState<any>([]);
+
+  const [search, setSearch] = useState('');
+  const [sortStatus, setSortStatus] = useState<DataTableSortStatus>({
+    columnAccessor: 'id',
+    direction: 'asc',
+  });
 
   useEffect(() => {
     setPage(1);
@@ -520,35 +527,60 @@ export const UserTable = () => {
   useEffect(() => {
     const from = (page - 1) * pageSize;
     const to = from + pageSize;
-    setRecordsData(rowData.slice(from, to));
-  }, [page, pageSize]);
+    setRecordsData([...initialRecords.slice(from, to)]);
+  }, [page, pageSize, initialRecords]);
+
+  useEffect(() => {
+    setInitialRecords(() => {
+      return rowData.filter((item) => {
+        return (
+          item.id.toString().includes(search.toLowerCase()) ||
+          item.firstName.toLowerCase().includes(search.toLowerCase()) ||
+          item.lastName.toLowerCase().includes(search.toLowerCase()) ||
+          item.email.toLowerCase().includes(search.toLowerCase()) ||
+          item.phone.toLowerCase().includes(search.toLowerCase())
+        );
+      });
+    });
+  }, [search]);
+
+  useEffect(() => {
+    const data = sortBy(initialRecords, sortStatus.columnAccessor);
+    setInitialRecords(sortStatus.direction === 'desc' ? data.reverse() : data);
+  }, [sortStatus]);
 
   return (
     <div className="panel mt-6">
-      <h5 className="mb-5 text-lg font-semibold dark:text-white-light">Basic</h5>
+      <div className="mb-5 flex flex-col gap-5 md:flex-row md:items-center">
+        <h5 className="text-lg font-semibold dark:text-white-light">Demo Table</h5>
+        <div className="ltr:ml-auto rtl:mr-auto">
+          <input type="text" className="form-input w-auto" placeholder="Search..." value={search} onChange={(e) => setSearch(e.target.value)} />
+        </div>
+      </div>
       <div className="datatables">
         <DataTable
-          noRecordsText="No results match your search query"
-          highlightOnHover
           className="table-hover whitespace-nowrap"
           records={recordsData}
           columns={[
-            { accessor: 'id', title: 'ID' },
-            { accessor: 'firstName', title: 'First Name' },
-            { accessor: 'lastName', title: 'Last Name' },
-            { accessor: 'email' },
-            { accessor: 'phone' },
+            { accessor: 'id', sortable: true },
+            { accessor: 'firstName', sortable: true },
+            { accessor: 'lastName', sortable: true },
+            { accessor: 'email', sortable: true },
+            { accessor: 'phone', title: 'Phone No.', sortable: true },
           ]}
-          totalRecords={rowData.length}
+          highlightOnHover
+          totalRecords={initialRecords.length}
           recordsPerPage={pageSize}
           page={page}
           onPageChange={(p) => setPage(p)}
           recordsPerPageOptions={PAGE_SIZES}
           onRecordsPerPageChange={setPageSize}
-          minHeight={200}
-          paginationText={({ from, to, totalRecords }) => `Showing  ${from} to ${to} of ${totalRecords} entries`}
+          sortStatus={sortStatus}
+          onSortStatusChange={setSortStatus}
           selectedRecords={selectedRecords}
           onSelectedRecordsChange={setSelectedRecords}
+          minHeight={200}
+          paginationText={({ from, to, totalRecords }) => `Showing  ${from} to ${to} of ${totalRecords} entries`}
         />
       </div>
     </div>
