e0a5c7558eab354247ea865615cf2cd0594379db
Muhammad Ali <ma496@outlook.com>
2026-02-14 15:27:47 +0500

    refactor: change any to Locale

--- src/frontend/web/app/[lang]/app/page.tsx
+++ src/frontend/web/app/[lang]/app/page.tsx
@@ -2,11 +2,11 @@ import { Metadata } from 'next'
 import SaleDashboard from './_components/sale-dashboard'
 
 import { getDictionary } from '../../../get-dictionary'
+import { Locale } from '@/i18n-config'
 
 export async function generateMetadata({ params }: { params: Promise<{ lang: string }> }): Promise<Metadata> {
   const { lang } = await params
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const dict = await getDictionary(lang as any)
+  const dict = await getDictionary(lang as Locale)
   return {
     title: dict.page.dashboard.title,
   }
